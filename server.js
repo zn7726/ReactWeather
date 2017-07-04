4223cd2502f5234fc25127be0157ba11a82b2d3f
Ethan Zhang <ezhang@rawideas.com>
2017-07-04 11:11:28 +1000

    remove redirect

--- server.js
+++ server.js
@@ -4,14 +4,6 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function(req, res, next){
-  if(req.headers['x-forwarded-proto' === 'https']){
-    next();
-  }else{
-    resp.redirect('https://'+req.hostname + req.url);
-  }
-});
-
 app.use(express.static('public'));
 
 app.listen(PORT, function (
